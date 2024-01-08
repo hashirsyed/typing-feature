3b33ff1fb1f4e32b517ff78b2d3ae2e9ce0d8da3
Hashir Syed <itshashirsyed@gmail.com>
2024-01-08 13:28:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var input = document.getElementById("input");
+var state = document.getElementById("tag");
+
+input.addEventListener('keydown',function(){
+    state.innerHTML = "Typing...";
+})
+input.addEventListener('keyup',function(){
+     timing = setTimeout(function() {
+        state.innerHTML = "Online";
+    }, 3000);
+})
